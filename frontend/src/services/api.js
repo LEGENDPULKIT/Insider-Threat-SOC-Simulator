d240622f4c65335db0d42d79d108c9d70bc28f3d
Akash Kiran Vernekar <akashoffi044@gmail.com>
2026-02-02 15:45:31 +0530

    Remove all files from repository

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = "http://127.0.0.1:8000";
-
-export const threatService = {
-  getLogs: () => axios.get(`${API_BASE}/view-logs`),
-  getAgentStatus: () => axios.get(`${API_BASE}/agent-status`)
-};
